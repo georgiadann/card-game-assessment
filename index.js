e202a1e3b3a0255b1aac446a78d5955a9dcbf085
Georgia Dann <78177141+georgiadann@users.noreply.github.com>
2021-08-11 02:29:00 +0000

    Fixed while loop and allowed player to pick how many lives they would like have in the game

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 
 console.log('Welcome to my card guessing game!');
 
+let playerLives = prompt('How many lives would you like to have?');
+while(playerLives <= 0 || isNaN(playerLives) || playerLives == null || playerLives % 1 != 0){
+  console.log('Not a valid number');
+  playerLives = prompt('How many lives would you like to have?');
+}
+
 // having list of player names ready to be picked
-  randomName1 = ['PickleBear64','progamer8290','Sheeplover22','gurglinggorilla',];
+  randomName1 = ['PickleBear64','progamer8290','Sheeplover22','gurglingGorilla',];
   let newRandomName1 = randomName1[Math.floor(Math.random()* 4 + 0 )];
 
-  randomName2 = ['tumblingT-rex','prancingpteridactol','sneakyhitman','chompingchimpanzee'];
+  randomName2 = ['tumblingT-rex','prancingPteridactol','sneakyHitman','chompingChimpanzee'];
   let newRandomName2 = randomName2[Math.floor(Math.random()* 4 + 0 )];
 
   //letting the player 1 choose their name
@@ -26,18 +32,18 @@ console.log('Welcome to my card guessing game!');
     console.log('Welcome ' + player2 + ', good luck!');
   }
 
-let name1 = (newRandomName1 || player1);
-let name2 = (newRandomName2 || player2);
+let name1 = (player1 || newRandomName1);
+let name2 = (player2 || newRandomName2);
 let pointPlayer1 = 0;
 let pointPlayer2 = 0;
-let player1Life = 3;
-let player2Life = 3;
-let lives = player1Life && player2Life;
+let player1Life = playerLives;
+let player2Life = playerLives;
 
-// while(lives<=1){
-//   playCard();
-// }
-playCard();
+
+while(player1Life>0 && player2Life>0){
+  playCard();
+}
+
 
 function playCard(){
   //randomizing the card numbers
@@ -47,11 +53,13 @@ function playCard(){
   newCard2 = Math.floor(Math.random() * 13 + 1);
 
   //player1 guessing their next card
-  let player1Guess= prompt(name1 + ' your card was a ' + card1 + '\nDo you think your next card will be higher or lower?');
-  while(player1Guess != 'higher'|| player1Guess!= 'lower'){
+  let player1Guess= prompt(name1 + ' your card was: ' + card1 + '\nDo you think your next card will be higher or lower?');
+
+  while(player1Guess != 'higher'&& player1Guess!= 'lower'){
     player1Guess = prompt('Do you think your next card will be higher or lower?');
   } 
-  console.log('You got a ' + newCard1);
+  console.log('You got: ' + newCard1);
+
   //setting player1s point outcome
   if(player1Guess == 'higher'){
     if(newCard1 > card1){
@@ -61,7 +69,7 @@ function playCard(){
       console.log('Better luck next time ' + name1 + ' you lose a life!');
       player1Life--;
     } else if(newCard1 == card1){
-      console.log('It\s a tie! ' + name1 + ' loses a life!');
+      console.log("It's a tie! " + name1 + ' loses a life!');
       player1Life--;
     }
   } else if(player1Guess == 'lower'){
@@ -72,23 +80,17 @@ function playCard(){
     console.log('Better luck next time ' + name1 + ' you lose a life!');
     player1Life--;
     } else if(newCard1 == card1){
-    console.log('It\s a tie! ' + name1 + ' loses a life!');
+    console.log("It's a tie! " + name1 + ' loses a life!');
     player1Life--;
     }
   }
-
-  let plural = pointPlayer1!=1?'s':'';
-  console.log(`${name1} has ${pointPlayer1} point${plural}\n${name1} has ${player1Life} lives left!`);
   console.log(name2 + ' Your turn!');
 
-
-
-
-  player2Guess= prompt(name2 + ' your card was a ' + card2 + '\nDo you think your next card will be higher or lower?');
-  while(player2Guess != 'higher' || player2Guess !='lower'){
+  player2Guess= prompt(name2 + ' your card was: ' + card2 + '\nDo you think your next card will be higher or lower?');
+  while(player2Guess != 'higher' && player2Guess !='lower'){
     player2Guess = prompt('Do you think your next card will be higher or lower?');
   }
-  console.log('You got a ' + newCard2);
+  console.log('You got: ' + newCard2);
   //setting player2s point outcome
   if(player2Guess == 'higher'){
     if(newCard2 > card2){
@@ -98,7 +100,7 @@ function playCard(){
       console.log('Better luck next time ' + name2 + ' you lose a life!');
       player2Life--;
     } else if(newCard2 == card2){
-      console.log('It\s a tie! ' + name2 + ' loses a life!');
+      console.log("It's a tie! " + name2 + ' loses a life!');
       player2Life--;
     }
   } else if(player2Guess == 'lower'){
@@ -109,11 +111,23 @@ function playCard(){
       console.log('Better luck next time ' + name2 + ' you lose a life!');
       player2Life--;
     } else if(newCard2 == card2){
-      console.log('It\s a tie! ' + name2 + ' loses a life!');
+      console.log("It's a tie! " + name2 + ' loses a life!');
       player2Life--;
   }
-} 
+  } 
 
-console.log(`${name2} has ${pointPlayer2} point${plural}\n${name2} has ${player2Life} lives left`);
+  //state what points people have
+  let plural = pointPlayer1!=1?'s':'';
+  console.log(`${name1} has ${pointPlayer1} point${plural}\n${name1} has ${player1Life} lives left!`);
+  console.log(`${name2} has ${pointPlayer2} point${plural}\n${name2} has ${player2Life} lives left`);
 
+}
+//States the winner of the game
+let plural = pointPlayer1!=1?'s':'';
+if (pointPlayer1 > pointPlayer2){
+  console.log(`Congratulations ${name1} you won with ${pointPlayer1} point${plural}`);
+} else if(pointPlayer1 < pointPlayer2){
+  console.log(`Congratulations ${name2} you won with ${pointPlayer2} point${plural}`);
+}else{
+  console.log("It's a tie!");
 }
